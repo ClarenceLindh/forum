fae2e93bc7152b6fd7cef7033b75ab89a0d144a7
AnnaProfile <72386803+AnnaProfile@users.noreply.github.com>
2022-01-19 22:23:18 +0100

    topiclist

--- frontend/src/Components/CreateThread.tsx
+++ frontend/src/Components/CreateThread.tsx
@@ -7,23 +7,34 @@ function CreateThread() {
 	
 	const [headL, setHeadL] = useState("");
 	const [content, setContent] = useState("");
+	const [theTopic, setTopic] = useState("");
 
 	const handleSubmit = () => {
 		
 	   alert(`headline: ${headL} content: ${content}`)
    
    }
+   const topicsList= [{"topic":"sport"},{"topic":"music"},{"topic":"art"}];
 
+   const pickTopic = () =>{
+	//document.getElementById("createThread").style.borderColor = "lightblue";
+	alert(`headline: ${theTopic} `)
+	
+} 
 
 			return (
-			<div className="createThread">
+			<div className="createThread" >
 				<h2>Create Thread</h2>
 				<form onSubmit={handleSubmit}>
   				
     				<input className="headlineThread" type="text" value={headL} onChange={(e) => setHeadL(e.target.value)} placeholder="Headline...." />
 					
 					<textarea className="contentThread" value={content} onChange={(e) => setContent(e.target.value)} placeholder="Write something...." />
-  					
+  					<div className="topicList">
+						  {topicsList.map(function(d,idx){
+							  return(<div id="topic" value={d.topic} onChange={(e) => setTopic(e.target.value)} key={idx}>{d.topic}</div>)
+						  })}
+					  </div>
   					<input className="submitThread" type="submit" value="Submit" />
 				</form>
 			</div>
