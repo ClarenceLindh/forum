6c6fc698b4b064a7edd988eb500a291009c292e9
FCO <franne_@hotmail.com>
2022-01-19 11:11:42 +0100

    changed to TopicEntity

--- backend/src/main/java/com/group1/forum/entities/TopicEntity.java
+++ backend/src/main/java/com/group1/forum/entities/TopicEntity.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "topics")
-public class Topic {
+public class TopicEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
