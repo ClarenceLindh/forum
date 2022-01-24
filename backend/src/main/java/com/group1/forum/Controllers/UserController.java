f5a8f899fcb96625cc16ed7289fa46ad79d70b36
Elias Rundqvist <rundqvistelias@gmail.com>
2022-01-24 14:34:29 +0100

    whoAmI

--- backend/src/main/java/com/group1/forum/Controllers/UserController.java
+++ backend/src/main/java/com/group1/forum/Controllers/UserController.java
@@ -27,7 +27,16 @@ public class UserController {
 
     @GetMapping("/auth/users")
     public List<UserEntity> getAllUsers(){return userService.getAll();}
-}
+
+    @GetMapping("/auth/whoami")
+    public UserEntity whoAmI() {
+        return userService.whoAmI();
+    }
+
+    }
+
+
+
 
 
 
