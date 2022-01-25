e508ab9d96f427c25a57d7304fd2661058e7481b
Stefan <Stefan_mijatovic97@hotmail.com>
2022-01-25 11:32:26 +0100

    more html

--- frontend/src/Components/Home.tsx
+++ frontend/src/Components/Home.tsx
@@ -1,49 +1,41 @@
 import React, { useEffect } from "react";
-import CreateThread from './CreateThread';
-import '../Styles/Home.scss';
+import CreateThread from "./CreateThread";
+import "../Styles/Home.scss";
 
-
-const topicsList= [{"topic":"sport"},{"topic":"music"},{"topic":"art"}];
-const List= [{"hmm":"test"},{"hmm":"test"},{"hmm":"test"}];
-
+const topicsList = [{ topic: "sport" }, { topic: "music" }, { topic: "art" }];
+const List = [{ hmm: "test" }, { hmm: "test" }, { hmm: "test" }];
 
 class Home extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        <header>
+          <div className="header">
+              <div></div>
+            <h1>FORUM</h1>
+            <h2>Sign in</h2>
+          </div>
 
- 
-	render() {
-		return (
-			<div className="main">
+          <div className="categories">
+            {topicsList.map(function (e) {
+              return <div id="topic">{e.topic}</div>;
+            })}
+          </div>
+        </header>
 
-                <header>
-                    <div>
-                    <h1>FORUM</h1>
-                <h2>Sign in</h2>
-                    </div>
 
-               
 
-                <div className="categories">
-						  {topicsList.map(function(d,idx){
-							  return(<div id="topic" key={idx}>{d.topic}</div>)
-							 // return(<div id="topic" value={d.topic} onChange={(e) => setTopic(e.target.value)} key={idx}>{d.topic}</div>)
-						  })}	 
-                          
-                           {List.map(function(d,idx){
-							  return(<div key={idx}>{d.hmm}</div>)
-							 // return(<div id="topic" value={d.topic} onChange={(e) => setTopic(e.target.value)} key={idx}>{d.topic}</div>)
-						  })}
-					    
-                </div>
-                </header>
+        <body>
+          <div className="items">
+            {List.map(function (e) {
+              return <div className="textItems">{e.hmm}</div>;
+            })}
+          </div>
+        </body>
 
-                <body>
-                
-                </body>
 
-				
- </div>
-        )
-        }
-    }
-    export default Home;
-          
+      </div>
+    );
+  }
+}
+export default Home;
