d8f0938effcd926d96fbc11272aa229288c72f70
FCO <franne_@hotmail.com>
2022-01-26 11:35:44 +0100

    working on getting thread by id (not done))

--- frontend/src/Components/ViewThread.tsx
+++ frontend/src/Components/ViewThread.tsx
@@ -3,6 +3,14 @@ import React, { useState } from "react";
 function ViewThread() {
     const [comment, setComment] = useState("");
 
+    const getThreadById = async (e: { preventDefault: () => void }) => {
+        e.preventDefault();
+    
+        // controller url: "/rest/thread/{threadId}"
+        const res = await fetch("/rest/thread/1", {});
+        console.log(res);
+      };
+
 
     return (
         <div className="threadContainer">
@@ -12,6 +20,9 @@ function ViewThread() {
             </div>
             <div className="threadContent">
                 <h3>CONTENT</h3>
+                <form onClick={getThreadById}>
+                    <button onClick={getThreadById}>Get Thread</button>
+                </form>
             </div>
             <div className="threadComment">
                 <h3>Comment here</h3>
