58e8f52070bbdd9256f59a803910e5411da77cdf
Stefan <Stefan_mijatovic97@hotmail.com>
2022-01-27 11:51:11 +0100

    Homepage

--- frontend/src/Components/Home.tsx
+++ frontend/src/Components/Home.tsx
@@ -1,22 +1,22 @@
 import React, {  useState } from "react";
 import "../Styles/Home.scss";
 import ThreadList from "./Threads/ThreadList";
+import CreateThread from "./CreateThread";
+
+
 
 const Home = () => {
-  return (
-    <div className="main">
-      <CreateThread />
-    </div>
-  );
-};
-
-const Home = () => {
- 
+ const test = true
   const [threads, setThreads] = useState([{id:1,name:'Todo1', complete:false },{id:1,name:'Todo2', complete:false },{id:1,name:'Todo3', complete:false },{id:1,name:'Todo4', complete:false },])
   const topicsList=[{topic:"sport"},{topic:"music"},{topic:"art"}]
     return (
 
       <div>
+
+<div className="main">
+
+   
+    </div>
      
      <div className="header">  
         <div></div>
