9dbc8c70b896fd374981585447fc46e90939526b
Stefan <Stefan_mijatovic97@hotmail.com>
2022-01-27 11:30:38 +0100

    Threads work

--- frontend/src/Components/Home.tsx
+++ frontend/src/Components/Home.tsx
@@ -3,28 +3,37 @@ import "../Styles/Home.scss";
 import ThreadList from "./Threads/ThreadList";
 
 
-
 const Home = () => {
  
-  const [threads, setThreads] = useState([{id:1,name:'Todo1', complete:false }])
-  
+  const [threads, setThreads] = useState([{id:1,name:'Todo1', complete:false },{id:1,name:'Todo2', complete:false },{id:1,name:'Todo3', complete:false },{id:1,name:'Todo4', complete:false },])
+  const topicsList=[{topic:"sport"},{topic:"music"},{topic:"art"}]
     return (
-      <div className="main">
 
-        <header>
-        <div className="header">
-              <div></div>
-            <h1>FORUM</h1>
-            <h2>Sign in</h2>
-          </div>
-        </header>
-
-        <ThreadList threadList={threads}/>
-
-    
-    
-
+      <div>
+     
+     <div className="header">  
+        <div></div>
+        <h1>Forum</h1>
+        <h2>Sign in</h2>
       </div>
+
+    
+      <body>
+        <div className="categories">
+          {topicsList.map(function(e){
+            return <div id="topic">{e.topic}</div>
+          })}
+        </div>
+
+        <div className="items">
+        <ThreadList threads={threads}/>
+
+        </div>
+        </body>
+      
+ 
+       </div>
+   
     );
   }
 
--- frontend/src/Components/Threads/ThreadList.tsx
+++ frontend/src/Components/Threads/ThreadList.tsx
@@ -2,46 +2,16 @@ import React from 'react';
 import Thread from './Thread';
 
 
-export default function ThreadList({ threadList }: {threadList: any[]}) {
+export default function ThreadList({threads}:{threads:any}) {
 
 
-    const topicsList = [{ topic: "sport" }, { topic: "music" }, { topic: "art" }];
-    // const List = [{ hmm: "test" }, { hmm: "test" }, { hmm: "test" }];
+
 
   return (
-    threadList.map(thread=>{
-      return<Thread key={thread.id} thread={thread}/>
-    })
-  // <div>
 
-
-
-        
-  //       <header>
-        
-
-  //         <div className="categories">
-  //           {topicsList.map(function (e) {
-  //             return <div id="topic">{e.topic}</div>;
-  //           })}
-  //         </div>
-  //       </header>
-
-
-
-  //       <body>
-  //         <div className='items'>
-         
-  //         </div>
-           
-  //         {/* <div className="items">
-  //           {List.map(function (e) {
-  //             return <div className="textItems">{e.hmm}</div>;
-  //           })}
-  //         </div> */}
-  //       </body>
-
-
-  //     </div>
-     );
+  threads.map((thread: { id: React.Key | null | undefined; })=>{
+    return <Thread key={thread.id} thread={thread}/>
+  })
+    
+    )
 }
