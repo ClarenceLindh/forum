c76036bcc6f7ed2a059f4d9c8ca5dee396f6c73a
Stefan <Stefan_mijatovic97@hotmail.com>
2022-01-31 11:28:24 +0100

    quickfix

--- frontend/src/Components/Threads/ThreadList.tsx
+++ frontend/src/Components/Threads/ThreadList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Thread from './Thread';
+import "../../Styles/Thread.scss";
 
 
 export default function ThreadList({threads}:{threads:any}) {
@@ -10,7 +11,7 @@ export default function ThreadList({threads}:{threads:any}) {
   return (
 
 
-<div>
+<div className='list'>
  { threads.map((thread: {  },index:number)=>{
     return <Thread key={index} thread={thread}/>
   })}
