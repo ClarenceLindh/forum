5308a031442ef8b1edf18615d0e47879b018297f
Stefan <Stefan_mijatovic97@hotmail.com>
2022-01-31 10:21:49 +0100

    v4

--- frontend/src/Components/Threads/ThreadList.tsx
+++ frontend/src/Components/Threads/ThreadList.tsx
@@ -10,7 +10,6 @@ export default function ThreadList({threads}:{threads:any}) {
   return (
 <div>
 
-  <h1>THREAD?</h1>
  { threads.map((thread: {  })=>{
     return <Thread  thread={thread}/>
   })}
