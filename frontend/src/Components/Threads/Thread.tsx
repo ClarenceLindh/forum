f0148e34d9e9b7e73673965854dc7f5e474ef76c
FCO <franne_@hotmail.com>
2022-02-01 16:40:07 +0100

    link-btn

--- frontend/src/Components/Threads/Thread.tsx
+++ frontend/src/Components/Threads/Thread.tsx
@@ -10,16 +10,16 @@ export default function Thread({ thread }: {thread: any}) {
 
     <div>
     <h2>{thread.title}</h2>
-      <div>{thread.id}</div>
-      <Link to={`/viewThread/${threadId}`} >
-        {thread.title}
-      </Link>
     </div>
 
     <div>
    {thread.text}
-    </div>
-<br /><br /><br /> <br />
+    </div>    
+<br />
+<div>
+  <button><Link  className='linkButton' to={`/viewThread/${threadId}`} >Go to thread</Link></button>
+</div>
+<br /><br /> <br />
 {thread.creationDate}
   </div>
   
