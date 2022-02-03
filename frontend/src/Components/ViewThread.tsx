dd8510817810b219dc7122828adb3ccc531d0e14
BilalAllouhaibi <bilalzmhaa99@gmail.com>
2022-02-03 11:37:49 +0100

    delete function with confirm window and try catch delete thread works
    Signed-off-by: BilalAllouhaibi <bilalzmhaa99@gmail.com>

--- frontend/src/Components/ViewThread.tsx
+++ frontend/src/Components/ViewThread.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import "../Styles/Thread.scss";
 import Thread from "./Threads/Thread";
 
 function ViewThread() {
-
+    const navigate = useNavigate();
     const [comment, setComment] = useState("");
     const {threadId} = useParams(); // 
     var [response] = useState<any>({});
@@ -22,7 +23,7 @@ function ViewThread() {
         
         console.log('this is response: ', response);
         console.log(res);
-        
+       
 
       };
       
@@ -31,18 +32,19 @@ function ViewThread() {
             
         }});
     }, [threadId]);
-          /*
-          Object.keys(data).map((obj, i) => {       return (         <div>           {data[obj].name}         </div>
-
-          */
-    
-
-    // controller url: "/rest/thread/{threadId}"
-    const raw = await fetch(`/rest/thread/${threadId}`);
-    const res = await raw.json();
-    console.log(res);
-  };
-
+      
+    let deleteThreadById = async ()=> {
+        if (window.confirm("are you sure you want to delete " + post.title ) == true){
+        try{
+            let response = await fetch(`/rest/thread/${threadId}`, {method: 'DELETE'})
+            console.log(response.status)
+            if(response.status == 200) navigate("/")
+        }catch(error){
+            alert("error try later")
+        }}else{
+            alert("you cancled the delete")
+        }
+      }     
     return (
         <div className="threadContainer">
             <br />
@@ -57,7 +59,8 @@ function ViewThread() {
                 <h3>Comment here</h3>
                 <textarea className="comment" value={comment} onChange={(e) => setComment(e.target.value)} placeholder="Comment..." />
                 <div>
-                    <button>Post</button>    
+                    <button>Post</button>
+                    <button onClick={deleteThreadById}>Delete</button>     
                 </div>
             </div>
             <br />
