fe870993cd1b326b1b425f725cfe02426f6961af
FCO <franne_@hotmail.com>
2022-02-03 22:15:03 +0100

    405..

--- frontend/src/Components/ViewThread.tsx
+++ frontend/src/Components/ViewThread.tsx
@@ -5,75 +5,77 @@ import { formatISO } from "date-fns";
 
 
 function ViewThread() {
+    const [commentId, setCommentId] = useState<any>({})
     const [comment, setComment] = useState<any>({});
     const commentDate = formatISO(new Date());
-    const {threadId} = useParams(); // 
+    const { threadId } = useParams(); // 
     var [response] = useState<any>({});
     const [post, setPost] = useState<any>({})
- 
+
+
     const getThreadById = async (e: { preventDefault: () => void; }) => {
-     e.preventDefault();
-    
-        // controller url: "/rest/thread/{threadId}"
+        e.preventDefault();
+
         const raw = await fetch(`/rest/thread/${threadId}`);
         const res = await raw.json();
         response = res;
-
         setPost(res);
-        
+
         console.log('this is response: ', response);
         console.log(res);
-        
 
-      };
 
-      const postComment = async (e: {preventDefault: () => void}) => {
-          e.preventDefault();
+    };
 
-          const commentDetails = {
-              commentId: {},
-              date: commentDate,
-              text: comment,
-              creator_id: {}
-          }
+    const postComment = async (e: { preventDefault: () => void }) => {
+        e.preventDefault();
 
-          try {
-              const response = await fetch("/rest/thread/comment", {
-                  method: "POST",
-                  headers: { "Content-Type": "application/json" },
-                  body: JSON.stringify(commentDetails)
-                })
-                const result = response.json();
-                console.log("this is result: ", result);
-                setComment(result);
+        const commentDetails = {
+            commentId: commentId,
+            date: commentDate,
+            text: comment,
+            //   creator_id: {}
+        }
 
-          } catch(error) {
-              return error;
-          }
-      }
+        try {
+            const response = await fetch("/rest/thread/comment", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(commentDetails)
+            })
+            // const result = response.json();
+            console.log("this is result: ", response);
+            setComment(response);
 
-      
-      useEffect( () => {
-        getThreadById({preventDefault: () => {
-            
-        }});
+        } catch (error) {
+            return error;
+        }
+    }
+
+
+    useEffect(() => {
+        getThreadById({
+            preventDefault: () => {
+
+            }
+        });
     }, [threadId]);
-          /*
-          Object.keys(data).map((obj, i) => {       return (         <div>           {data[obj].name}         </div>
+    /*
+    Object.keys(data).map((obj, i) => {       return (         <div>           {data[obj].name}         </div>
 
-          */
-    
+    */
+
 
 
     return (
         <div className="threadContainer">
             <br />
-            <div className="threadTitle">  
+            <div className="threadTitle">
                 {post.title}
-            <br />
+                <br />
             </div>
             <div className="threadContent">
-                {post.text} 
+                {post.text}
             </div>
             <br />
             <div className="commentView">
@@ -86,8 +88,8 @@ function ViewThread() {
                     <textarea className="comment" onChange={(e) => setComment(e.target.value)} placeholder="Comment..." />
                     <button onClick={postComment}>Post</button>
                 </form>
-            
-            
+
+
 
             </div>
             <br />
