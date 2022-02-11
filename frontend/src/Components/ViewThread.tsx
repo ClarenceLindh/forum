81cfd006f758e5a8a0cf8551ccd3c6781d3063f0
FCO <franne_@hotmail.com>
2022-02-11 23:27:54 +0100

    Don't merge

--- frontend/src/Components/ViewThread.tsx
+++ frontend/src/Components/ViewThread.tsx
@@ -99,6 +99,8 @@ function ViewThread() {
           comments.shift();
           tuggle = true;
           console.log('COMMENTSSSSSSSSSSSSSSSS: ', comments)
+
+          // DONT MERGE
         }
       })
     )
