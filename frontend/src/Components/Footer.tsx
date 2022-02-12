32a81e9c3805376110ca3e8318bfc0a10013f6d4
Elias Rundqvist <rundqvistelias@gmail.com>
2022-02-12 22:15:18 +0100

    blocked list

--- frontend/src/Components/Footer.tsx
+++ frontend/src/Components/Footer.tsx
@@ -6,7 +6,23 @@ import { Context } from "../Context/ContextProvider";
 
 const Footer = () => {
   const { loggedInUser, whoAmI } = useContext(Context);
+  const [banned, setBanned] = useState ([{}])
 
+  async function fetchBlocked() {
+    
+    const raw = await fetch(`/rest/users/allBlockedUsers`);
+    const res = await raw.json();
+    console.log(res);
+
+    res.forEach((element: { id: any; name: string; complete: boolean }) => {
+      setBanned((banned) => [...banned, element]);
+    });
+  }
+
+    useEffect( ()=> {
+      fetchBlocked();
+      console.log("dis is ban", banned)
+    }, []);
 
   return (
     <div className="inFooter">
@@ -17,8 +33,8 @@ const Footer = () => {
       <div>My treads</div>
       {loggedInUser.role === "ROLE_ADMIN" ? (
                     <div>
-                      Admin Ban List
-                    </div>
+                      admin list
+                  </div>
                 ): (
                     <a>
                       
@@ -27,5 +43,6 @@ const Footer = () => {
     </div>
   );
 };
+
 
 export default Footer;
