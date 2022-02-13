1a727215c182b3ab61e7151dfe98c4cef2383677
Elias Rundqvist <rundqvistelias@gmail.com>
2022-02-13 21:31:59 +0100

    remove knapp fixad

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -1,11 +1,46 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router";
 import { Context } from "../Context/ContextProvider";
 
 function Header() {
   const navigate = useNavigate();
+  var [response] = useState<any>({});
 
   const { logout, loggedInUser } = useContext(Context);
+
+  let deleteAccountByClick = async () => {
+    const accountInfo = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            deleted: true,
+        }),
+    };
+    if (
+        window.confirm("are you sure you want to delete Your account") ==
+        true
+    ) {
+        try {
+            await fetch(`/rest/editDelete/${loggedInUser.id}`, accountInfo).then(
+                async (response) => {
+                    const data = await response.json();
+                    logout()
+                    alert("User deleted")
+                }
+                
+            );
+
+            
+            if (response.status == 200) navigate("/");
+        } catch (error) {
+            alert("error, try later");
+        }
+    } else {
+        alert("you canceled the delete");
+    }
+};
 
   return (
     <div className="header">
@@ -24,6 +59,7 @@ function Header() {
           <div className="loginLogoutContainer">
             <h3>Welcome back {loggedInUser.username}!</h3>
             <button className="logoutButton noButtonCss" onClick={logout}>logout</button>
+            <button onClick={deleteAccountByClick}> Delete account </button>
           </div>
         )}
       </div>
